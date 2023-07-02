025a147633e463fcbe171929a20cb4a72c4e16f1
appitsolution <applicationitsolution@gmail.com>
2023-07-02 13:52:49 +0300

    added cabinet

--- js/cabinet.js
+++ js/cabinet.js
@@ -8,6 +8,20 @@ const openInformationHistory = ({ target }) => {
   }
 };
 
+const openInformationOrder = ({ target }) => {
+  if (target.classList.contains("cabinet-information__order-info-button")) {
+    if (target.parentElement.parentElement.classList.contains("active")) {
+      target.parentElement.parentElement.classList.remove("active");
+    } else {
+      target.parentElement.parentElement.classList.add("active");
+    }
+  }
+};
+
 document
   .querySelector(".cabinet-information__history-list")
   .addEventListener("click", openInformationHistory);
+
+document
+  .querySelector(".cabinet-information__order-list")
+  .addEventListener("click", openInformationOrder);
